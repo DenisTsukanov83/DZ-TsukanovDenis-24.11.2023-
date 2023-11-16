fe11372a5ad7b3eaedae64a25a619503621d9556
DenisTsukanov83 <90137616+DenisTsukanov83@users.noreply.github.com>
2023-11-16 13:48:36 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+$( document ).ready(function() {
+
+    $('.about .about-nav ul li a').click((e) => {
+        e.preventDefault();
+        $('.about .contents').hide();
+        $('.about .about-menu ul li a').removeClass('active');
+        e.target.classList.add('active');
+        
+        switch(true) {
+            case e.target.textContent == 'About me': $('.about-content').show();
+            break;
+            case e.target.textContent == 'Resume': $('.resume-content').show();
+            break;
+            case e.target.textContent == 'Testimonials': $('.testimon-content').show();
+            break;
+            case e.target.textContent == 'Contact': $('.contact-content').show();
+            break;
+            case e.target.textContent == 'Portfolio': $('.portfolio-content').show();
+            break;
+        }
+    });
+
+    $('.portfolio-nav ul li a').click((e) => {
+        e.preventDefault();
+        $('.portfolio-nav ul li a').removeClass('active').addClass('not-active');
+        e.target.classList.remove('not-active');
+        e.target.classList.add('active');
+        $('.portfolio-grid').hide();
+        $('.portfolio-grid').removeClass('grid');
+
+        switch(true) {
+            case e.target.textContent == 'All': $('.portfolio-grid-all').addClass('grid');
+            break;
+            case e.target.textContent == 'Graphic design': $('.portfolio-grid-graphic').addClass('grid');
+            break;
+            case e.target.textContent == 'Web design': $('.portfolio-grid-web').addClass('grid');
+            break;
+            case e.target.textContent == 'Photography': $('.portfolio-grid-photography').addClass('grid');
+            break;
+        }
+    });
+});
